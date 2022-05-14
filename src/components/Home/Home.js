ebb57bf3837b380a39182fe1bf902ff5a9d40082
eid sid <eidsiddiedis@gmail.com>
2022-05-14 17:58:20 +0200

    working on home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,16 @@
 import Header from "../Header/Header";
+import RidesShow from "../RidesShow/RidesShow";
+import useFetch from "../global/FetchAPI/Fetch";
 function Home() {
+  let ridesApi = "https://assessment.api.vweb.app/rides";
+  let userApi = "https://assessment.api.vweb.app/user";
+  let user = useFetch(userApi);
+  let rides = useFetch(ridesApi);
+  console.log(rides, user);
   return (
     <div className="home">
-      <h1>this is home</h1>
+      <Header {...user} />
+      <RidesShow rides={rides} stationcode={user.station_code} />
     </div>
   );
 }
